8d512b6138f8f1fe0457c0b809bf729aebc8172d
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-16 15:10:38 +0300

    servis ve component oluşturuldu

---
+++ PratikForumClient/src/app/bank-account/bank-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-account',
+  templateUrl: './bank-account.component.html',
+  styleUrls: ['./bank-account.component.css']
+})
+export class BankAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
