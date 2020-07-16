76f30982151f60701b565b61b256ce8c667287d8
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-16 14:02:10 +0300

    init

---
+++ PratikForumAPI/PratikForumAPI/Models/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PratikForumAPI.Models
+{
+    public class BankAccount
+    {
+    }
+}
