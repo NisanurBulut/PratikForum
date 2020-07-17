453e831ebd83d8bdea83904017ea63753c21d701
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-17 10:38:04 +0300

    bank bilgileri için component yazıldı.

--- PratikForumClient/src/app/app-routing.module.ts
+++ PratikForumClient/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BankAccountComponent } from './bank-account/bank-account.component';
 import { HomeComponent } from './home/home.component';
+import { BankComponent } from './bank/bank.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'bankAccount', component: BankAccountComponent },
+    { path: 'bank', component: BankComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
