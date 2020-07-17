a04cb9e6cba3a5988093cb842fcef28499593ba2
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-17 15:03:36 +0300

    kullanıcı işlemleri tanımlandı

--- PratikForumClient/src/app/shared/user.service.ts
+++ PratikForumClient/src/app/shared/user.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
+import { catchError } from 'rxjs/internal/operators/catchError';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUserList() {
+    return this.http.get(environment.apiBaseUrl + '/Bank')
+      .pipe(catchError(this.handleError));
+  }
+  deleteUser(id) {
+    return this.http.delete(environment.apiBaseUrl + '/Bank/' + id)
+      .pipe(catchError(this.handleError));
+  }
+  postUser(formData: FormGroup) {
+    return this.http.post(environment.apiBaseUrl + '/Bank', formData)
+      .pipe(catchError(this.handleError));
+  }
+  putUser(formData: any) {
+    return this.http.put(environment.apiBaseUrl + '/Bank/' + formData.bankID, formData)
+      .pipe(catchError(this.handleError));
+  }
+  private handleError(error: any, caught: any): any {
+    console.log(error);
+  }
 }
--- PratikForumClient/src/app/user/user.component.ts
+++ PratikForumClient/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, Validators, FormGroup } from '@angular/forms';
+import { UserService } from '../shared/user.service';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  notification = null;
+  userForms: FormArray = this.fb.array([]);
+  constructor(private fb: FormBuilder, private us: UserService) { }
 
   ngOnInit() {
   }
+  GetUserList() {
+    this.userForms.clear();
 
+    this.us.getUserList().subscribe(res => {
+      console.log(res);
+      if (res === [] || (res as []).length === 0) {
+        this.AddUserForm();
+      } else {
+        (res as []).forEach((user: any) => {
+          this.userForms.push(this.fb.group({
+            userID: [user.userID, Validators.min(1)],
+            name: [user.Name],
+            surname: [user.Surname, Validators.required],
+            password: [user.Password, Validators.required]
+          }));
+        });
+      }
+    });
+  }
+  AddUserForm() {
+    this.userForms.push(this.fb.group({
+      userID: [0, Validators.min(1)],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      password: ['', Validators.required]
+    }));
+  }
+  recordSubmit(fg: FormGroup) {
+    if (fg.get('userID').value === 0) {
+      this.us.postUser(fg.value).subscribe(
+        (res: any) => {
+          fg.patchValue({ userID: res.userID });
+        }
+      );
+    } else {
+      this.us.putUser(fg.value).subscribe(
+        (res: any) => {
+          this.GetUserList();
+          this.showNotification('update');
+        }
+      );
+    }
+  }
+  deleteUser(userID, i: number) {
+    if (userID === 0) {
+      this.userForms.removeAt(i);
+    } else if (confirm('Bu işlemi yapmak istediğinizden emin misiniz ?')) {
+      this.us.deleteUser(userID).subscribe(
+        (res: any) => {
+          this.userForms.removeAt(i);
+          this.showNotification('delete');
+        }
+      );
+    }
+  }
+  showNotification(category) {
+    switch (category) {
+      case 'insert':
+        this.notification = { class: 'text-success', message: 'Başarıyla kaydedildi.' };
+        break;
+      case 'update':
+        this.notification = { class: 'text-warning', message: 'Başarıyla güncellendi.' };
+        break;
+      case 'delete':
+        this.notification = { class: 'text-danger', message: 'Başarıyla silindi.' };
+        break;
+      default:
+        break;
+    }
+    setTimeout(() => {
+      this.notification = null;
+    }, (3000));
+  }
 }
