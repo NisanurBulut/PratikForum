d2dfca7d42f9d09b374bc16dd6fc96ef7559191d
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-23 17:58:26 +0300

    routing düzenlemesi

--- PratikForumClient/src/app/app-routing.module.ts
+++ PratikForumClient/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BankAccountComponent } from './bank-account/bank-account.component';
 import { HomeComponent } from './home/home.component';
 import { BankComponent } from './bank/bank.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { UserOperationsComponent } from './user/user-operations/user-operations.component';
 import { BankOperationsComponent } from './bank/bank-operations/bank-operations.component';
 import { BankDegerlendirmeComponent } from './bank/bank-degerlendirme/bank-degerlendirme.component';
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -16,19 +21,20 @@ const routes: Routes = [
           { path: 'degerlendirme/:id', component: BankDegerlendirmeComponent }
         ]
       },
-    // {
-    //     path: 'bank',
-    //     component: BankComponent
-    // },
-    // {
-    //     path: 'bank/operations',
-    //     component: BankOperationsComponent
-    // },
-    // {
-    //     path: 'bank/degerlendirme/:id',
-    //     component: BankDegerlendirmeComponent
-    // },
-
+    {
+        path: 'user', component: UserComponent,
+        children: [
+            {
+                path: 'registration', component: RegistrationComponent, pathMatch: 'full',
+            },
+            {
+                path: 'login', component: LoginComponent, pathMatch: 'full',
+            },
+            {
+                path: 'operations', component: UserOperationsComponent, pathMatch: 'full',
+            }
+        ]
+    },
     { path: 'bankAccount', component: BankAccountComponent }
 ];
 
