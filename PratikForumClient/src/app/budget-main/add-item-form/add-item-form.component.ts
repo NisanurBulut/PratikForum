23eb51658c2af5fb75357558aa8a9d7853547abc
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-26 22:33:06 +0300

    bütçe için form hazırlandı.

--- PratikForumClient/src/app/budget-main/add-item-form/add-item-form.component.ts
+++ PratikForumClient/src/app/budget-main/add-item-form/add-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item-form',
@@ -11,5 +12,7 @@ export class AddItemFormComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm): void {
+    alert('Merhaba Nisanur');
+  }
 }
