505c1acb22ea8e8e6976eebb30c1b04b45ecea2b
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-26 22:56:56 +0300

    form olayı

--- PratikForumClient/src/app/budget-main/add-item-form/add-item-form.component.ts
+++ PratikForumClient/src/app/budget-main/add-item-form/add-item-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/app/models/budget-item.model';
 
 @Component({
   selector: 'app-add-item-form',
@@ -7,9 +8,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-item-form.component.css']
 })
 export class AddItemFormComponent implements OnInit {
-
   constructor() { }
-
+  @Input() item: BudgetItem = new BudgetItem('', null);
   ngOnInit() {
   }
   onSubmit(form: NgForm): void {
