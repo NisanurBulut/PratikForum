28e3d90bd2cdf9e0bfa2f1594634cb0f45238cb0
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-26 22:28:24 +0300

    ana ekran tasarımı tamamlandı.

--- PratikForumClient/src/app/budget-main/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ PratikForumClient/src/app/budget-main/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-card',
   templateUrl: './budget-item-card.component.html',
-  styleUrls: ['./budget-item-card.component.css']
+  styleUrls: ['./budget-item-card.component.scss']
 })
 export class BudgetItemCardComponent implements OnInit {
-
+  @Input() isIncome: boolean;
   constructor() { }
 
   ngOnInit() {
