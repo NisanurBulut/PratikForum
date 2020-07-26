6b5d3ae3b9f88f783d49e5cb682efaaf45900796
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-26 22:41:32 +0300

    budget-item model yazıldı.

---
+++ PratikForumClient/src/app/models/budget-item.model.ts
@@ -0,0 +1,14 @@
+// export class BudgetItem {
+//     description: string;
+//     amount: number;
+//     constructor(description: string, amount: number) {
+//         this.description = description;
+//         this.amount = amount;
+//     }
+// }
+
+export class BudgetItem {
+
+    constructor(public description: string, public amount: number) {
+    }
+}
