19ca36b7fa588f7c945464a0da1e2155a90ae132
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-07 19:42:38 +0300

    bulma eklemesi düzeltildi.

--- PratikForumClient/src/app/app.module.ts
+++ PratikForumClient/src/app/app.module.ts
@@ -25,7 +25,7 @@ import { AddItemFormComponent } from './budget-main/add-item-form/add-item-form.
 import { BudgetItemListComponent } from './budget-main/budget-item-list/budget-item-list.component';
 import { BudgetItemCardComponent } from './budget-main/budget-item-list/budget-item-card/budget-item-card.component';
 import { EditItemModalComponent } from './budget-main/edit-item-modal/edit-item-modal.component';
-import {TaskViewComponent} from './task-pages/task-view/task-view.component';
+
 
 @NgModule({
   declarations: [
@@ -47,8 +47,7 @@ import {TaskViewComponent} from './task-pages/task-view/task-view.component';
     AddItemFormComponent,
     BudgetItemListComponent,
     BudgetItemCardComponent,
-    EditItemModalComponent,
-    TaskViewComponent
+    EditItemModalComponent
   ],
   imports: [
     BrowserModule,
