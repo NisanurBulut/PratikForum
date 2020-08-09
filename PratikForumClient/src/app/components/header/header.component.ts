302835be4fe501164636384a2717601906cdaea1
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-09 18:09:14 +0300

    readme güncellendi.

--- PratikForumClient/src/app/components/header/header.component.ts
+++ PratikForumClient/src/app/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   // dışarıya bilgi vermeli
--- PratikForumClient/src/app/layouts/default/default.module.ts
+++ PratikForumClient/src/app/layouts/default/default.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DefaultComponent } from './default.component';
 import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 
@@ -13,7 +12,6 @@ import { ComponentModule } from 'src/app/components/component/component.module';
 
 @NgModule({
   declarations: [
-    DefaultComponent,
     DashboardComponent,
     PostsComponent
   ],
