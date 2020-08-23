5cf825d6b4bb04cafd4e974177c450e0df1b2873
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-23 20:52:43 +0300

    satır boşluk silme

--- PratikForumClient/src/app/app.module.ts
+++ PratikForumClient/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { UserOperationsComponent } from './user/user-operations/user-operations.component';
